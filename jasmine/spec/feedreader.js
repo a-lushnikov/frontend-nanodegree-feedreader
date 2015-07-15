f332eb509d24609c21f8fd7f6890c6cdc6bc63a9
Anatoliy Lushnikov <toliantm@narod.ru>
2015-07-15 18:18:36 +0200

    test: Added JS behavior tests using Jasmine
    RSS Feeds
    - are defined
    - no url is empty
    - no name is empty
    
    The menu
    - is hidden by default
    - changes visibility when menu icon is clicked
    
    Initial Entries
    - has at least a single entry within .feed container after loadFeed()
    
    New Feed Selection
    - changes content when a new feed is loaded

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,127 +1,158 @@
-/* feedreader.js
- *
- * This is the spec file that Jasmine will read and contains
- * all of the tests that will be run against your application.
- */
-
-/* We're placing all of our tests within the $() function,
- * since some of these tests may require DOM elements. We want
- * to ensure they don't run until the DOM is ready.
- */
-
-// var customMatchers = {
-//     toBeDefinedAndNotEmpty: function(util, customEqualityTesters) {
-//         return {
-//             compare: function(actual, expected) {
-//                 if (expected === undefined) {
-//                     expected = '';
-//                 }
-//                 var result = {};
-//                 result.pass = util.equals(actual.hyuk, "gawrsh" + expected, customEqualityTesters);
-//                 if (result.pass) {
-//                     result.message = "Expected " + actual + " not to be quite so goofy";
-//                 } else {
-//                     result.message = "Expected " + actual + " to be goofy, but it was not very goofy";
-//                 }
-//                 return result;
-//             }
-//         };
-//     }
-// };
+'use strict';
 
 $(function() {
-    /* This is our first test suite - a test suite just contains
-     * a related set of tests. This suite is all about the RSS
-     * feeds definitions, the allFeeds variable in our application.
-     */
     describe('RSS Feeds', function() {
 
-        // beforeEach(function() {
-        //     jasmine.addMatchers(customMatchers);
-        // });
-
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
-         */
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
-            expect(allFeeds.length).not.toBe(0);
+            expect(allFeeds.length).toBeGreaterThan(0);
         });
 
-
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
         it('no url is empty', function() {
             for (var i = allFeeds.length - 1; i >= 0; i--) {
                 expect(allFeeds[i].url).toBeTruthy();
-            };
+            }
         });
-
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
 
         it('no name is empty', function() {
             for (var i = allFeeds.length - 1; i >= 0; i--) {
                 expect(allFeeds[i].name).toBeTruthy();
-            };
+            }
         });
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
-    xdescribe('The menu', function() {
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
-        it('element is hidden by default', function() {
+    describe('The menu', function() {
+        var trans1 = '';
+        var trans2 = '';
 
+        beforeAll(function(){
+            // save initial transition css
+            trans1 = $('.menu').css('transition');
+            trans2 = $('.menu-hidden .menu').css('transition');
+
+            // remove async transition
+            $('.menu').css('transition', 'transform 0.0s');
+            $('.menu-hidden .menu').css('transition', 'transform 0.0s');
         });
 
-        /* TODO: Write a test that ensures the menu changes
-         * visibility when the menu icon is clicked. This test
-         * should have two expectations: does the menu display when
-         * clicked and does it hide when clicked again.
-         */
+        it('is hidden by default', function() {
+            expect($('.menu:in-viewport')).not.toExist();
+        });
+
         it('changes visibility when menu icon is clicked', function() {
+            // check if opens
+            $('.menu-icon-link').click();
+            expect($('.menu:in-viewport')).toExist();
 
+            // check if closes
+            $('.menu-icon-link').click();
+            expect($('.menu:in-viewport')).not.toExist();
+        });
+
+        afterAll(function() {
+            // restore transition values
+            $('.menu').css('transition', trans1);
+            $('.menu-hidden .menu').css('transition', trans2);
         });
     });
 
 
 
-    /* TODO: Write a new test suite named "Initial Entries" */
-    xdescribe('Initial Entries', function() {
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test wil require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
-        it('ensures that there is at least a single entry within .feed container after loadFeed()', function() {
+    describe('Initial Entries', function() {
+        beforeEach(function(done) {
+            // spyOn(window.google.feeds, 'Feed').and.callFake(function() {
+            //     // return mock object so to unit test our code in isolation
+            //     var mockObject = {
+            //         'error': '0',
+            //         'feed': {
+            //             'feedUrl': 'http://blog.udacity.com/feeds/posts/default?alt=rss',
+            //             'title': 'Udacity Blog',
+            //             'link': 'http://blog.udacity.com/',
+            //             'author': '',
+            //             'description': 'Advance your education and your career with project-based, self-paced online courses in tech.',
+            //             'type': 'rss20',
+            //             'entries': [{
+            //                 'title': '4 Times Data Science Saved the Day',
+            //                 'link': 'http://blog.udacity.com/2014/12/4-times-data-science-saved-day_3.html',
+            //                 'author': 'noreply@blogger.com (Mark Nguyen)',
+            //                 'publishedDate': 'Wed, 03 Dec 2014 10:31:00 -0800',
+            //                 'contentSnippet': '',
+            //                 'content': '',
+            //                 'categories': ['Data Science']
+            //             }, {
+            //                 'title': 'Dawoon Choi: Golfer to Programmer',
+            //                 'link': 'http://blog.udacity.com/2014/11/student-stories-dawoon-choi-programmer.html',
+            //                 'author': 'noreply@blogger.com (ChiWei Ranck)',
+            //                 'publishedDate': 'Fri, 28 Nov 2014 07:26:00 -0800',
+            //                 'contentSnippet': '',
+            //                 'content': '',
+            //                 'categories': ['front-end web dev', 'Nanodegrees', 'Student Stories']
+            //             }, {
+            //                 'title': 'Data Analysts: What You\'ll Make and Where You\'ll Make It',
+            //                 'link': 'http://blog.udacity.com/2014/11/data-analysts-what-youll-make.html',
+            //                 'author': 'noreply@blogger.com (Allison Stadd)',
+            //                 'publishedDate': 'Wed, 26 Nov 2014 08:00:00 -0800',
+            //                 'contentSnippet': '',
+            //                 'content': '',
+            //                 'categories': ['Careers', 'Data Science']
+            //             }, {
+            //                 'title': 'Informational Interviews: How to Find Your Next Job Over Coffee',
+            //                 'link': 'http://blog.udacity.com/2014/11/informational-interviews-how-to-find.html',
+            //                 'author': 'noreply@blogger.com (Allison Jones)',
+            //                 'publishedDate': 'Fri, 21 Nov 2014 08:50:00 -0800',
+            //                 'contentSnippet': '',
+            //                 'content': '',
+            //                 'categories': ['Careers']
+            //             }],
+            //         },
+            //         load: function(cb) {
+            //             if (cb) {
+            //                 cb();
+            //             }
+            //         }
+            //     };
+            //     return mockObject;
+            // });
 
+            // async call to loadFeed which will get fake object
+            loadFeed(0, function() {
+                done();
+            });
+        });
+
+        it('has at least a single entry within .feed container after loadFeed()', function(done) {
+            expect($('.feed a').length).toBeGreaterThan(0);
+            done();
         });
     });
 
-
-
-
-    /* TODO: Write a new test suite named "New Feed Selection" */
 
     describe('New Feed Selection', function() {
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
+        var preloaded = [];
 
+        // preload the first feed
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                preloaded = $('.feed a article h2').map(function(i, el){
+                    return el.innerHTML;
+                }).sort();
+                done();
+            });
+        });
+
+        it('changes content when a new feed is loaded', function(done) {
+            var validation = function() {
+                var loaded = $('.feed a article h2').map(function(i, el){
+                    return el.innerHTML;
+                }).sort();
+
+                expect(loaded).not.toEqual(preloaded);
+                done();
+            };
+
+            // load the second feed and compare to the first one
+            loadFeed(1, validation);
+        });
     });
 }());
